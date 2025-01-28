c8c384700aad5faff51f823cf9a58215b197ab51
William Koller <williamkoller30@gmail.com>
2025-01-28 16:24:11 -0300

    feat: add output

--- local.tf
+++ local.tf
@@ -6,3 +6,15 @@ resource "local_file" "exemplo" {
 variable "conteudo" {
   type = string
 }
+
+output "id-do-arquivo" {
+  value = resource.local_file.exemplo.id
+}
+
+output "conteudo-do-arquivo" {
+  value = var.conteudo
+}
+
+output "chicken-egg" {
+  value = sort(["🐔","🥚"])
+}
