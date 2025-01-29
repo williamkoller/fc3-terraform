7d00c76a98522f8aee2154be88fe68335f7d7080
William Koller <williamkoller30@gmail.com>
2025-01-29 13:05:22 -0300

    feat: add cluster and nodes

--- cluster.tf
+++ cluster.tf
@@ -39,3 +39,25 @@ resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
   role = aws_iam_role.cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_cloudwatch_log_group" "log" {
+  name = "/aws/eks/${var.prefix}-${var.cluster_name}/cluster"
+  retention_in_days = var.retention_days
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name = "${var.prefix}-${var.cluster_name}"
+  role_arn = aws_iam_role.cluster.arn
+  enabled_cluster_log_types = ["api","audit"]
+
+  vpc_config {
+    subnet_ids = aws_subnet.subnets[*].id
+    security_group_ids = [ aws_security_group.new-sg.id ]
+  }
+
+  depends_on = [ 
+    aws_cloudwatch_log_group.log,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy
+  ]
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,6 @@
 prefix = "fullcycle"
 cluster_name = "curso-fullcycle"
+retention_days = 30
+desired_size = 2
+max_size = 4
+min_size = 2
