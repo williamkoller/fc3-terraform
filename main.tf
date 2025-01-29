2ff9581f58811d519dd9a2fc4e67aacb1c48c7a5
William Koller <williamkoller30@gmail.com>
2025-01-29 15:36:17 -0300

    feat: move providers

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 0.13.1"
+  
+  required_providers {
+    aws = ">= 3.54.0"
+    local = ">= 2.1.0"
+  }
+
+  backend "s3" {
+    bucket = "wkfcbucket"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "new-vpc" {
   source = "./modules/vpc"
   prefix = var.prefix
